5f0a2cc501b474880f6fa766360d963b2073dd77
Audran <audran.bert@alumni.univ-avignon.fr>
2021-04-24 00:00:12 +0200

    scene Home update launch and the difficulties menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,8 +5,10 @@ import ai.MultiLayerPerceptron;
 import ai.SigmoidalTransferFunction;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -16,7 +18,10 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import static ai.Test.loadCoupsFromFile;
@@ -25,7 +30,7 @@ import static sample.Settings.difficulty;
 
 //import javax.swing.*;
 
-public class Controller {
+public class Controller implements Initializable  {
 
     @FXML
     private Button lauchButton;
@@ -54,8 +59,36 @@ public class Controller {
     private double lr = 0.0;
     private int l = 0;
 
+    public void initialize(URL location, ResourceBundle resources) {
+        makeMenuLoad();
+    }
 
+    public void makeMenuLoad(){
+        Settings settings=new Settings();
+        ArrayList<String> diff=settings.readDifficulties();
+        difficultyButton.getItems().clear();
+        if (diff!=null){
+            for (int i=0;i<diff.size();i++){
+                MenuItem m=new MenuItem("Load "+diff.get(i));
+                m.setId(diff.get(i));
+                difficultyButton.getItems().add(m);
+                m.setOnAction(loadMenu);
 
+            }
+        }
+    }
+
+    EventHandler<ActionEvent> loadMenu = new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent e)
+        {
+            System.out.println(((MenuItem)e.getSource()).getText() + " selected");
+            Settings settings=new Settings();
+            difficulty=((MenuItem)e.getSource()).getId();
+            settings.launchIA(((MenuItem)e.getSource()).getId(),mainProgressBar);
+        }
+    };
+
+    /*
     public void chooseEasy(){
         System.out.println("Facile");
         difficultyButton.setText("Facile");
@@ -72,10 +105,14 @@ public class Controller {
         settings.readConf("D");
 
     }
+*/
 
     public void launchaGame(){
+        System.out.println("Chargement de la partie");
+        //System.out.println(difficulty);
+        Settings settings=new Settings();
+        settings.readConf(difficulty);
         System.out.println("Lancement de la partie");
-        System.out.println(difficulty);
     }
 
     public void updateText() {
