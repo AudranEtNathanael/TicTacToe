514e84dbc9b631b396f367a8712a0e4e74ad7cd7
Nathanael-Lefevre <nathanael.lefevre@alumni.univ-avignon.fr>
2021-05-16 11:10:02 +0200

    Nettoyage du code de ControllerGrid, ajout de commentaire et correction de l'écran de fin en cas d'égalité

--- src/sample/ControllerGrid.java
+++ src/sample/ControllerGrid.java
@@ -2,7 +2,6 @@ package sample;
 
 import ai.Coup;
 import ai.MultiLayerPerceptron;
-import ai.SigmoidalTransferFunction;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,14 +10,10 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
@@ -29,18 +24,20 @@ import static sample.Settings.getFile;
 
 public class ControllerGrid implements Initializable {
 
+    // Boutons de contrôle
     @FXML private ImageView homeButton;
     @FXML private Button buttonPlayAgain;
     @FXML private Button buttonQuit;
 
+    // Ecran de fin  (Sur la meme scène pour avoir la partie en arrière plan)
     @FXML private Rectangle backgroundHider;
     @FXML private VBox backgroundHiderHolder;
-
     @FXML private Label labelVictoryOf;
     @FXML private Label textVictoryOf;
     @FXML private Label labelAgainst;
     @FXML private Label textAgainst;
 
+    // PlaceHolders non séléctionné pour les pions
     @FXML private ImageView placeHolder0;
     @FXML private ImageView placeHolder1;
     @FXML private ImageView placeHolder2;
@@ -53,6 +50,7 @@ public class ControllerGrid implements Initializable {
 
     private List<ImageView> placeHolders;
 
+    // PlaceHolders séléctionné pour les pions
     @FXML private ImageView placeHolderSelected0;
     @FXML private ImageView placeHolderSelected1;
     @FXML private ImageView placeHolderSelected2;
@@ -65,6 +63,7 @@ public class ControllerGrid implements Initializable {
 
     private List<ImageView> placeHoldersSelected;
 
+    // Pions croix
     @FXML private ImageView cross0;
     @FXML private ImageView cross1;
     @FXML private ImageView cross2;
@@ -77,6 +76,7 @@ public class ControllerGrid implements Initializable {
 
     private List<ImageView> crosses;
 
+    // pions cercle
     @FXML private Circle circle0;
     @FXML private Circle circle1;
     @FXML private Circle circle2;
@@ -97,6 +97,7 @@ public class ControllerGrid implements Initializable {
 
     private double[] gameState;  // contient des null, Circle circleX ou des ImageView crossX (avec x in 0..8)
 
+    // Liste des cases adjacentes à vérifier pour chaque case. (évite des calculs et évite d'avoir plein de if) Si la grille étit plus grande, on procéderait autrement avec des fonction modulaires.
     private int[][][] posToVerify = {{{6, 3}, {1, 2}, {4, 8}},
                                      {{0, 2}, {4, 7}},
                                      {{0, 1}, {5, 8}, {4, 6}},
@@ -107,58 +108,44 @@ public class ControllerGrid implements Initializable {
                                      {{1, 4}, {6, 8}},
                                      {{2, 5}, {6, 7}, {0, 4}}};
 
-
+    // l'ia utilisée pour jouer
     private MultiLayerPerceptron ai;
 
-    private static ControllerGrid CG;
-
-    public static ControllerGrid getCG(){
-        return CG;
-
-    }
 
     public ControllerGrid(){
-        CG = this;
-
         gameState = new double[9];
-        for(int i=0; i<9; i++){
+        for(int i=0; i<gameState.length; i++){
             gameState[i] = Coup.EMPTY;
         }
-
-        int[] layers = new int[Settings.getL()+2];
-        layers[0]=9;
-        for (int i=0; i<Settings.getL(); i++){
-            layers[i+1] = Settings.getH();
-        }
-        layers[layers.length-1]=9;
-
-        //ai = new MultiLayerPerceptron(layers, Settings.getLr(), new SigmoidalTransferFunction());
-        System.out.println("File for ai is : " + getFile());
+        System.out.println("File to load for ai in Game is : " + getFile());
         ai = MultiLayerPerceptron.load(getFile());
 
     }
 
 
+    /**
+     *
+     * @param url
+     * @param resourceBundle
+     * Méthode appellée à l'initialisation de la scène
+     * Met en place les éléments de la scène et configure les eventHandlers
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Arrived on grille");
 
-        backgroundHiderHolder.setVisible(false);
-        backgroundHider.setVisible(false);
-        labelVictoryOf.setVisible(false);
-        textVictoryOf.setVisible(false);
-        labelAgainst.setVisible(false);
-        textAgainst.setVisible(false);
-
-        buttonPlayAgain.setVisible(false);
-        buttonQuit.setVisible(false);
-
-        pionToPlay = true;
-
+        // à ne pas mettre directement dans le constructeur : ces listes pourraient être nulle dans le corps de cette fonction (erreur testée).
         placeHolders = Arrays.asList(placeHolder0,placeHolder1, placeHolder2, placeHolder3, placeHolder4, placeHolder5, placeHolder6, placeHolder7, placeHolder8);
         placeHoldersSelected = Arrays.asList(placeHolderSelected0, placeHolderSelected1, placeHolderSelected2, placeHolderSelected3, placeHolderSelected4, placeHolderSelected5, placeHolderSelected6, placeHolderSelected7, placeHolderSelected8);
         crosses = Arrays.asList(cross0, cross1, cross2, cross3, cross4, cross5, cross6, cross7, cross8);
         circles = Arrays.asList(circle0, circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8);
+
+        pionToPlay = true;
+
+        // cache l'écran de fin et les pions
+        hideWinningScreen();
+        for (ImageView currentCross : crosses) currentCross.setVisible(false);
+        for (Circle currentCircle : circles) currentCircle.setVisible(false);
 
 
         // un placeHolder survolé affiche le placeHolderSelected correspondant
@@ -174,13 +161,14 @@ public class ControllerGrid implements Initializable {
             });
         }
 
+
         // placeHoldersSelected caché par defaut
         // un placeHolderSelected non survolé se cache à nouveau
-        // un placeHolderSelected cliqué affiche le bon pion correspondant, se cache et cache le placeHolder correspondant
+        // un placeHolderSelected cliqué fait jouer le pion correspondant puis l'ia, le cas échéant
         for (int i=0; i<placeHoldersSelected.size(); i++){
             placeHoldersSelected.get(i).setVisible(false);
 
-            int finalI = i;
+            int finalI = i;  // finalI rends la valeur de i accessible par les eventHandlers
             placeHoldersSelected.get(i).addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
@@ -194,95 +182,52 @@ public class ControllerGrid implements Initializable {
                 @Override
                 public void handle(MouseEvent event) {
                     System.out.println("PlaceHolder n°" + String.valueOf(finalI) + " clicked");
-                    placeHoldersSelected.get(finalI).setVisible(false);
-                    placeHolders.get(finalI).setVisible(false);
-                    if (aiGameMode) {
+
+                    // On joue le pion cliqué
+                    hidePlaceholder(finalI);
+                    if (pionToPlay) {
                         crosses.get(finalI).setVisible(true);
                         gameState[finalI] = Coup.X;
                     } else {
-                        if (pionToPlay) {
-                            crosses.get(finalI).setVisible(true);
-                            gameState[finalI] = Coup.X;
-                        } else {
-                            circles.get(finalI).setVisible(true);
-                            gameState[finalI] = Coup.O;
-                        }
+                        circles.get(finalI).setVisible(true);
+                        gameState[finalI] = Coup.O;
                     }
 
                     displayGameState();
-                    System.out.println("pion to play : " + (pionToPlay ? "Croix" : "Cercles"));
-                    if (isGameFinished(finalI) == null) {  // Si la partie n'est pas finie, l'autre joueur ou l'IA joue.
+
+                    // On vérifie si la partie est finie. Si non, on passe fait jouer l'IA le cas échéant ou on laisse lautre joueur jouer
+                    int[] isGameFinishedResult = isGameFinished(finalI);
+                    if (isGameFinishedResult == null) {  // Si la partie n'est pas finie, l'autre joueur ou l'IA joue.
                         pionToPlay = !pionToPlay;
                         int aiPionPLayed = -1;
                         if (aiGameMode){
                             aiPionPLayed = playAi();
-                            if (aiPionPLayed != -1 && isGameFinished(aiPionPLayed) != null) {  // Si on ne peut plus jouer
-                                for (int i : isGameFinished(aiPionPLayed)) {
-                                    System.out.println(i);
-                                }
-                                hidePlaceholders();
-                                backgroundHiderHolder.setVisible(true);
-                                backgroundHider.setVisible(true);
-                                System.out.println("les " + (pionToPlay ? "croix" : "cercles") + " (IA) ont gagné");
+                            isGameFinishedResult = isGameFinished(aiPionPLayed);
+                            if (aiPionPLayed != -1 && isGameFinishedResult != null) {  // Si on ne peut plus jouer
+                                for (int i : isGameFinishedResult) System.out.println(i);
+                                showWinningScreen(isGameFinishedResult);
                             }
                             pionToPlay = !pionToPlay;
                         }
-
-
                     }else{  // Si on ne peut plus jouer
-                        for(int i : isGameFinished(finalI)){
-                            System.out.println(i);
-                        }
-                        hidePlaceholders();
-                        backgroundHiderHolder.setVisible(true);
-                        backgroundHider.setVisible(true);
-                        System.out.println("les " + (pionToPlay ? "Croix" : "Cercles") + " ont gagné");
-                        textVictoryOf.setText(pionToPlay ? "Croix" : "Cercles");
-                        textAgainst.setText(pionToPlay ? "Cercles" : "Croix");
-                        textVictoryOf.setTextFill(Color.web(pionToPlay ? "#3481eb" : "#d7292e"));
-                        textAgainst.setTextFill(Color.web(pionToPlay ? "#d7292e" : "#3481eb"));
-                        labelVictoryOf.setVisible(true);
-                        labelAgainst.setVisible(true);
-                        textVictoryOf.setVisible(true);
-                        textAgainst.setVisible(true);
-
-                        buttonPlayAgain.setVisible(true);
-                        buttonQuit.setVisible(true);
+                        for(int i : isGameFinishedResult) System.out.println(i);
+                        showWinningScreen(isGameFinishedResult);
                     }
                     event.consume();
                 }
             });
         }
-
-        for (ImageView currentCross : crosses){
-            currentCross.setVisible(false);
-        }
-
-        for (Circle currentCircle : circles){
-            currentCircle.setVisible(false);
-        }
     }
 
-    public void hidePlaceholders(){
-        for (int i=0; i<placeHoldersSelected.size(); i++) {
-            placeHoldersSelected.get(i).setVisible(false);
-        }
-        for (int i=0; i<placeHolders.size(); i++) {
-            placeHolders.get(i).setVisible(false);
-        }
-    }
 
-    public void showPlaceholders(){
-        for (int i=0; i<placeHolders.size(); i++) {
-            if(gameState[i] == Coup.EMPTY) {
-                placeHolders.get(i).setVisible(true);
-            }
-        }
-    }
-
+    /**
+     * @return
+     * Fais jouer l'ia
+     * retourne la position du pion joué par l'ia
+     */
     private int playAi() {
         System.out.println("C'est au tour de l'ia");
-        hidePlaceholders();
+        hideAllPlaceholders();
 
         double[] gameStateIaOut = ai.forwardPropagation(gameState);
         for(int i=0; i<gameStateIaOut.length; i++){
@@ -291,13 +236,13 @@ public class ControllerGrid implements Initializable {
 
         int caseToPlay = 0;
         int i=0;
-        for(; i<gameStateIaOut.length; i++){
+        for(; i<gameStateIaOut.length; i++){  // Recherche de la première position jouable
             if(gameState[i] == 0){
                 caseToPlay = i;
                 break;
             }
         }
-        for(; i<gameStateIaOut.length; i++){
+        for(; i<gameStateIaOut.length; i++){  // Recherche de la position jouable dont le score est le plus élevé
             if ((gameStateIaOut[i] > gameStateIaOut[caseToPlay]) && (gameState[i] == 0)){
                 caseToPlay = i;
             }
@@ -311,6 +256,33 @@ public class ControllerGrid implements Initializable {
         return caseToPlay;
     }
 
+    /**
+     * @param posPLayed
+     * @return
+     *
+     * Retourne la position des pions gagnants
+     * Retourne null si personne n'as gagné
+     * Retourne la position de toute les cases si la partie est finie et que personne n'as gagné
+     */
+    private int[] isGameFinished(int posPLayed){
+        int intPionToPlay = pionToPlay ? Coup.X : Coup.O;
+        for(int[] currentPosToVerify : posToVerify[posPLayed]){
+            System.out.println(intPionToPlay + " " + gameState[currentPosToVerify[0]] + " " + gameState[currentPosToVerify[1]]);
+            if(gameState[currentPosToVerify[0]] == intPionToPlay && gameState[currentPosToVerify[1]] == intPionToPlay){
+                return new int[]{posPLayed, currentPosToVerify[0], currentPosToVerify[1]};
+            }
+        }
+
+        for(int i=0; i<gameState.length; i++){
+            if(gameState[i] == Coup.EMPTY) return null;  // Si on peut encore jouer
+        }
+
+        return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8};  // Si il n'y a pas de gagnant, on retourne toute les cases pour les animer
+    }
+
+    /**
+     * Affiche l'état du jeu
+     */
     private void displayGameState() {
         for(int i=0; i<gameState.length; i++){
             if(i%3 == 0){
@@ -328,23 +300,80 @@ public class ControllerGrid implements Initializable {
     }
 
 
+    /**
+     * @param gameFinishedResult
+     * Affiche l'écran de fin avec le bon texte
+     */
+    private void showWinningScreen(int[] gameFinishedResult) {
+        hideAllPlaceholders();
+        backgroundHiderHolder.setVisible(true);
+        backgroundHider.setVisible(true);
 
-    private int[] isGameFinished(int posPLayed){
-        int intPionToPlay = pionToPlay ? Coup.X : Coup.O;
-        for(int[] currentPosToVerify : posToVerify[posPLayed]){
-            System.out.println(intPionToPlay + " " + gameState[currentPosToVerify[0]] + " " + gameState[currentPosToVerify[1]]);
-            if(gameState[currentPosToVerify[0]] == intPionToPlay && gameState[currentPosToVerify[1]] == intPionToPlay){
-                return new int[]{posPLayed, currentPosToVerify[0], currentPosToVerify[1]};
-            }
+        if(gameFinishedResult.length != 9) {
+            System.out.println("les " + (pionToPlay ? "Croix" : "Cercles") + " ont gagné");
+            textVictoryOf.setText(pionToPlay ? "Croix" : "Cercles");
+            textAgainst.setText(pionToPlay ? "Cercles" : "Croix");
+        }else{System.out.println("Persone n'as gagné");
+            textVictoryOf.setText("Personne");
+            textAgainst.setText("Le destin..");
         }
+        textVictoryOf.setTextFill(Color.web(pionToPlay ? "#3481eb" : "#d7292e"));
+        textAgainst.setTextFill(Color.web(pionToPlay ? "#d7292e" : "#3481eb"));
+        labelVictoryOf.setVisible(true);
+        labelAgainst.setVisible(true);
+        textVictoryOf.setVisible(true);
+        textAgainst.setVisible(true);
 
-        for(int i=0; i<gameState.length; i++){
-            if(gameState[i] == Coup.EMPTY) return null;  // Si on peut encore jouer
-        }
-
-        return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};  // Si il n'y a pas de gagnant, on retourne toute les cases pour les animer
+        buttonPlayAgain.setVisible(true);
+        buttonQuit.setVisible(true);
     }
 
+    /**
+     * Cache l'écran de fin
+     */
+    private void hideWinningScreen() {
+        backgroundHiderHolder.setVisible(false);
+        backgroundHider.setVisible(false);
+        labelVictoryOf.setVisible(false);
+        textVictoryOf.setVisible(false);
+        labelAgainst.setVisible(false);
+        textAgainst.setVisible(false);
+
+        buttonPlayAgain.setVisible(false);
+        buttonQuit.setVisible(false);
+    }
+
+    /**
+     * Affiche tous les placeHolder pour lesquels aucun pion n'as été posé
+     */
+    public void showPlaceholders(){
+        for (int i=0; i<placeHolders.size(); i++) {
+            if(gameState[i] == Coup.EMPTY) {
+                placeHolders.get(i).setVisible(true);
+            }
+        }
+    }
+
+    /**
+     * @param placeHolderNumber
+     * Cache le placeHolder placeHolderNumber
+     */
+    private void hidePlaceholder(int placeHolderNumber) {
+        placeHoldersSelected.get(placeHolderNumber).setVisible(false);
+        placeHolders.get(placeHolderNumber).setVisible(false);
+    }
+
+    /**
+     * Cache tous les placeHolder
+     */
+    public void hideAllPlaceholders(){
+        for (int i=0; i<placeHoldersSelected.size(); i++) {
+            placeHoldersSelected.get(i).setVisible(false);
+        }
+        for (int i=0; i<placeHolders.size(); i++) {
+            placeHolders.get(i).setVisible(false);
+        }
+    }
 
     public void changePageToPageSample() throws IOException {
         PageLoader.changePage("../view/home.fxml", this);
@@ -352,16 +381,5 @@ public class ControllerGrid implements Initializable {
 
     public void changePageToGrid() throws IOException {
         PageLoader.changePage("../view/InGame/grid.fxml", this);
-    }
-
-
-    public void addCircle() throws IOException {
-        System.out.println("Vous posez un pion Cercle");
-        placeHoldersSelected.get(0).setVisible(true);
-    }
-
-    public void unselectPlaceHolder() throws IOException {
-        System.out.println("placeHolder désélectionné");
-        placeHoldersSelected.get(0).setVisible(false);
     }
 }
